020d118cdd45b686c1afaa29023c9e1060574d0d
LuisDaniel45 <luisdanielhernandezlopez@gmail.com>
2023-10-13 15:40:00 -0500

    added a sigin path

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"text/template"
 )
 
@@ -15,17 +16,63 @@ func main()  {
         "index.html",
         "main.html",
         "error.html",
+        "register.html",
+        "login.html",
     );
     if err != nil {panic(err) }
 
 
-    var counter int;
-    var str []string;
+    session_manager := make(map[string]string) 
+    users := make(map[string]string) 
+    todo := make(map[string][]string);
+    var user_counter int;
+
     http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+        if r.Method == "POST" {
+            r.ParseForm();
+            if r.PostForm.Has("username") && 
+               r.PostForm.Has("password") {
+                   user := r.PostForm["username"][0];
+                   if _, ok := users[user]; ok ||  
+                   user == ""  || r.PostForm["password"][0] == "" {
+                       t.ExecuteTemplate(w, "error.html", err_values{400, 
+                       "Bad Request, username already taken, or missing input"})
+                       return
+                   }
+
+                   users[user] = r.PostForm["password"][0];
+                   user_counter++;
+
+                   uuid, err := os.ReadFile("/proc/sys/kernel/random/uuid")
+                   if err != nil {
+                       println("ERROR: generating uuid for session_id error")
+                       t.ExecuteTemplate(w, "error.html", err_values{501, "Internal Server Error"})
+                       return
+                   }
+
+                   session_id := string(uuid[:len(uuid)-1])
+                   session_manager[session_id] = user  
+                   http.SetCookie(w, &http.Cookie{
+                       Name: "auth", 
+                       Value: session_id, 
+                   });
+
+                   http.Redirect(w, r, "/", 301);
+                   return
+            }
+        }
+
+        err = t.ExecuteTemplate(w, "register.html", nil);
+        if err != nil {
+            println("ERROR: register render");
+        }
     });
 
     http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-
+        err = t.ExecuteTemplate(w, "login.html", nil);
+        if err != nil {
+            println("ERROR: login render");
+        }
     });
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -33,20 +80,40 @@ func main()  {
             w.WriteHeader(404)
             err = t.ExecuteTemplate(w, "error.html", err_values{404, "Not Found"})
             if err != nil {
-                println("ERROR: content render");
+                println("ERROR: render error.html");
             }
             return;
         }
 
+        cookie, err := r.Cookie("auth");
+        if err != nil {
+            w.WriteHeader(403)
+            err = t.ExecuteTemplate(w, "error.html", err_values{403, "Forbidden Acces, Not Autherize"})
+            if err != nil {
+                println("ERROR: render errror.html");
+            }
+            return 
+        }
+
+        user, ok := session_manager[cookie.Value];
+        if !ok {
+            w.WriteHeader(403)
+            err = t.ExecuteTemplate(w, "error.html", err_values{403, "Forbidden Access, Not Autherize"})
+            if err != nil {
+                println("ERROR: render errror.html");
+            }
+            return
+        }
+
+
         if r.Method == "POST" {
             r.ParseForm();
             if r.PostForm.Has("todo") {
-                str = append(str, r.PostForm["todo"][0])
-                counter++;
+                todo[user] = append(todo[user], r.PostForm["todo"][0])
             }
         }
 
-        err = t.ExecuteTemplate(w, "main.html", str)
+        err = t.ExecuteTemplate(w, "main.html", todo[user])
         if err != nil {
             println("ERROR: content render");
         }
